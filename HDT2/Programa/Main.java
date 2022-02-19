22a7c5393f9c128864d74dc47d02f8c9f14042f2
LINDA JIMÉNEZ <lijv1333@gmail.com>
2022-02-18 23:45:16 -0600

    Modificaciones

---
+++ HDT2/Programa/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Vista vista = new Vista();
+        CalcuPostfix cp = new CalcuPostfix();
+
+        cp.leerOperacion();
+
+    }
+}
---
+++ HDT2/Programa/Stack.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Stack implements InterfaceStack{
+    
+    protected ArrayList<String> stack =  new ArrayList<String>();
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        System.out.println("Stack vacio!");
+        return size() == 0;
+        
+    }
+
+    @Override
+    public void push(String item) {
+        // TODO Auto-generated method stub
+        stack.add(item);
+        
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return stack.size();
+    }
+
+    @Override
+    public void pop() {
+        // TODO Auto-generated method stub
+        if(stack.isEmpty()){
+            System.out.println("Stack vacio!");
+            return;
+        } else{
+            stack.remove(stack.size() - 1);
+        }
+    }
+}
+
