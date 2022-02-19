f1e144dda9b5857b05dfe66e8dc740809dd2d516
LINDA JIMÉNEZ <lijv1333@gmail.com>
2022-02-19 13:45:41 -0600

    Modificaciones finales Main

--- HDT2/Programa/Main.java
+++ HDT2/Programa/Main.java
@@ -1,10 +1,28 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         
         Vista vista = new Vista();
         CalcuPostfix cp = new CalcuPostfix(); 
 
-        cp.leerOperacion();
+        int opcion = 0;
 
+       while (opcion != 3){
+
+           opcion = vista.menu();
+
+           if (opcion == 1){
+
+            cp.leerOperacion();
+
+           } else if (opcion == 2){
+
+            vista.insertarExpresion();
+
+           } else if (opcion == 3){
+
+                vista.salir();
+
+           }
+        }   
     }
 }
