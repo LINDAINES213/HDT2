e000125ff1c44cfeb8a8aaffb118b1a23a56eb65
LINDA JIMÉNEZ <lijv1333@gmail.com>
2022-02-19 16:18:47 -0600

    Test Calculadora y Stack

---
+++ HDT2/Programa/TestCalcu.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+import junit.framework.TestCase;
+
+public class TestCalcu extends TestCase {
+	
+	private CalcuPostfix cp = new CalcuPostfix();
+	private Stack S = new Stack();
+
+	public void testevaluarOperacion() {
+		 assertTrue(cp.evaluarOperacion("2 3 +") == (2+3));
+	}
+	
+	public void testpush() {
+		
+		Stack<Integer> stack2 = new Stack<Integer>();
+		stack2.push(2);
+		assertTrue(stack2.push(2) == stack2.peek());
+	}
+	
+	public void testpeek() {
+		Stack<Integer> stack2 = new Stack<Integer>();
+		stack2.push(2);
+		assertTrue(stack2.push(2) == stack2.peek());
+	}
+	
+	public void testIsEmpty() {
+		Stack<Integer> stack2 = new Stack<Integer>();
+		assertEquals(true, stack2.isEmpty());
+	}
+	
+	public void testsize() {
+		Stack<Integer> stack2 = new Stack<Integer>();
+		stack2.push(2);
+		stack2.push(8);
+		assertEquals(2, stack2.size());
+	}
+	
+}
